b218040f8ea44de1f49943365fc208b02fdf26f8
Lakshmana selva v <99866508+lakshmanaselvan@users.noreply.github.com>
2023-08-15 11:42:16 +0530

    Add files via upload

---
+++ Hand Tracking/main.py
@@ -0,0 +1,25 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+
+detector = HandDetector(detectionCon=0.8, maxHands=1)
+video = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = video.read()
+    frame = cv2.flip(frame, 1)
+    hands, img = detector.findHands(frame)
+
+    if hands:
+        lmList = hands[0]
+        fingerUp = detector.fingersUp(lmList)
+        fingerCount = fingerUp.count(1)
+
+        cv2.putText(frame, f'Finger count: {fingerCount}', (20, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
+
+    cv2.imshow("frame", frame)
+    k = cv2.waitKey(1)
+    if k == ord("k"):
+        break
+
+video.release()
+cv2.destroyAllWindows()
